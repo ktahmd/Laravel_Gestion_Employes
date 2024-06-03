cd4c4e8a975aa523777a9b90cd9ff3f52154196c
Lekhsaraethman <168539902+Lekhsaraethman@users.noreply.github.com>
2024-06-03 23:19:17 +0000

    done

--- database/migrations/2024_06_03_225302_create_departements_table.php
+++ database/migrations/2024_06_03_225302_create_departements_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('departements', function (Blueprint $table) {
             $table->id();
+            $table->string('nom', 50)
             $table->timestamps();
         });
     }
