919201fe6e8f8355d881f570f9a0e5f2ed5e4209
Lekhsaraethman <168539902+Lekhsaraethman@users.noreply.github.com>
2024-06-04 11:34:11 +0000

    h

--- app/Models/Departements.php
+++ app/Models/Departements.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Departements extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        
+        'nom',
+    ];
+
+  
 }
+
+
