b4c20544585f2c98141fa5e162cf991323fb75d9
Lekhsaraethman <168539902+Lekhsaraethman@users.noreply.github.com>
2024-06-04 19:02:06 +0000

    b

--- database/migrations/2024_06_03_225211_create_messages_table.php
+++ database/migrations/2024_06_03_225211_create_messages_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+               
+           
+            $table->strig('contenu',255);
+           
+            $table->string('employe_id')->unsigned();
+            $table->string('employe_id_1')->unsigned();
+            $table->timestamps();
+            $table->foreign('employe_id')->references('id')->on('Employess')->onDelete('cascade');
+            $table->foreign('employe_id_1')->references('id')->on('employe_id')->onDelete('cascade');
+           
             $table->timestamps();
         });
     }
