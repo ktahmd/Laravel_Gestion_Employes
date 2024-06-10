7833d7b4a2c7fce2dc19ead1c4ddff558c00d128
Lekhsaraethman <168539902+Lekhsaraethman@users.noreply.github.com>
2024-06-10 18:36:15 +0000

    jj

--- resources/views/gestionPersonnel/cv.blade.php
+++ resources/views/gestionPersonnel/cv.blade.php
@@ -1,3 +1,16 @@
+@extends('layouts.master')
+@section ('contenu')
+@if(session('success'))
+<div class="alert alert-success"role-"alert">
+{{session(' success')}}
+</div> 
+@endif
+@if (session ('faild'))
+<div class="alert alert-danger" role="alert">
+{{session('faild')}}
+</div> 
+@endif
+
 <div class="card">
                                     <div class="card-body">
         
