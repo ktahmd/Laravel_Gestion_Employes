04e56f89126f7b5aba275ba251cac3c2a51567d1
Lekhsaraethman <168539902+Lekhsaraethman@users.noreply.github.com>
2024-06-10 18:01:10 +0000

    OK

---
+++ resources/views/gestionPersonnel/cv.blade.php
@@ -0,0 +1,219 @@
+<div class="card">
+                                    <div class="card-body">
+        
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="invoice-title">
+                                                    <h4 class="float-end font-size-16"><strong>Order # 12345</strong></h4>
+                                                    <h3>
+                                                        <img src="assets/images/logo-sm.png" alt="logo" height="24"/>
+                                                    </h3>
+                                                </div>
+                                                <hr>
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <address>
+                                                            <strong>Billed To:</strong><br>
+                                                            John Smith<br>
+                                                            1234 Main<br>
+                                                            Apt. 4B<br>
+                                                            Springfield, ST 54321
+                                                        </address>
+                                                    </div>
+                                                    <div class="col-6 text-end">
+                                                        <address>
+                                                            <strong>Shipped To:</strong><br>
+                                                            Kenny Rigdon<br>
+                                                            1234 Main<br>
+                                                            Apt. 4B<br>
+                                                            Springfield, ST 54321
+                                                        </address>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-6 mt-4">
+                                                        <address>
+                                                            <strong>Payment Method:</strong><br>
+                                                            Visa ending **** 4242<br>
+                                                            jsmith@email.com
+                                                        </address>
+                                                    </div>
+                                                    <div class="col-6 mt-4 text-end">
+                                                        <address>
+                                                            <strong>Order Date:</strong><br>
+                                                            January 16, 2019<br><br>
+                                                        </address>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+        
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div>
+                                                    <div class="p-2">
+                                                        <h3 class="font-size-16"><strong>Order summary</strong></h3>
+                                                    </div>
+                                                    <div class="">
+                                                        <div class="table-responsive">
+                                                            <table class="table">
+                                                                <thead>
+                                                                <tr>
+                                                                    <td><strong>Item</strong></td>
+                                                                    <td class="text-center"><strong>Price</strong></td>
+                                                                    <td class="text-center"><strong>Quantity</strong>
+                                                                    </td>
+                                                                    <td class="text-end"><strong>Totals</strong></td>
+                                                                </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                <!-- foreach ($order->lineItems as $line) or some such thing here -->
+                                                                <tr>
+                                                                    <td>BS-200</td>
+                                                                    <td class="text-center">$10.99</td>
+                                                                    <td class="text-center">1</td>
+                                                                    <td class="text-end">$10.99</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>BS-400</td>
+                                                                    <td class="text-center">$20.00</td>
+                                                                    <td class="text-center">3</td>
+                                                                    <td class="text-end">$60.00</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>BS-1000</td>
+                                                                    <td class="text-center">$600.00</td>
+                                                                    <td class="text-center">1</td>
+                                                                    <td class="text-end">$600.00</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td class="thick-line"></td>
+                                                                    <td class="thick-line"></td>
+                                                                    <td class="thick-line text-center">
+                                                                        <strong>Subtotal</strong></td>
+                                                                    <td class="thick-line text-end">$670.99</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td class="no-line"></td>
+                                                                    <td class="no-line"></td>
+                                                                    <td class="no-line text-center">
+                                                                        <strong>Shipping</strong></td>
+                                                                    <td class="no-line text-end">$15</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td class="no-line"></td>
+                                                                    <td class="no-line"></td>
+                                                                    <td class="no-line text-center">
+                                                                        <strong>Total</strong></td>
+                                                                    <td class="no-line text-end"><h4 class="m-0">$685.99</h4></td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+        
+                                                        <div class="d-print-none">
+                                                            <div class="float-end">
+                                                                <a href="javascript:window.print()" class="btn btn-success waves-effect waves-light"><i class="fa fa-print"></i></a>
+                                                                <a href="#" class="btn btn-primary waves-effect waves-light ms-2">Send</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+        
+                                            </div>
+                                        </div> <!-- end row -->
+        
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div> <!-- end row -->
+
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+                
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>document.write(new Date().getFullYear())</script> © Upcube.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-end d-none d-sm-block">
+                                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        <div class="right-bar">
+            <div data-simplebar class="h-100">
+                <div class="rightbar-title d-flex align-items-center px-3 py-4">
+            
+                    <h5 class="m-0 me-2">Settings</h5>
+
+                    <a href="javascript:void(0);" class="right-bar-toggle ms-auto">
+                        <i class="mdi mdi-close noti-icon"></i>
+                    </a>
+                </div>
+
+                <!-- Settings -->
+                <hr class="mt-0" />
+                <h6 class="text-center mb-0">Choose Layouts</h6>
+
+                <div class="p-4">
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="layout-1">
+                    </div>
+
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input theme-choice" type="checkbox" id="light-mode-switch" checked>
+                        <label class="form-check-label" for="light-mode-switch">Light Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="layout-2">
+                    </div>
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input theme-choice" type="checkbox" id="dark-mode-switch" data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css">
+                        <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="layout-3">
+                    </div>
+                    <div class="form-check form-switch mb-5">
+                        <input class="form-check-input theme-choice" type="checkbox" id="rtl-mode-switch" data-appStyle="assets/css/app-rtl.min.css">
+                        <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
+                    </div>
+
+            
+                </div>
+
+            </div> <!-- end slimscroll-menu-->
+        </div>
+        <!-- /Right-bar -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <script src="assets/js/app.js"></script>
+
+
+    </body>
+</html>
